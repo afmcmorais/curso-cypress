cc74cf9fd0abde70d5be97a3ac8f70ae5fb31924
André Felipe <afmcmorais@icloud.com>
2023-11-14 14:04:33 -0300

    first commit

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,22 @@
+/// reference types="cypress" />
+
+describe('Cypress basics', () => {
+    it('Should visit a page and assert title', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+    })
+
+    it('Should find and interact with a element', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+    })
+})
---
+++ cypress/e2e/describe.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+it('A external test...', () => {
+
+})
+
+describe('Should group tests', () => {
+    describe('Should group more specifc tests...', () => {
+        it('A specific test...', () => {
+
+        })
+        
+        it('A specific test 2...', () => {
+
+        })
+    })
+
+    describe('Should group more specifc tests 2...', () => {
+        it('A specific test...', () => {
+
+        })
+        
+        it('A specific test 2...', () => {
+
+        })
+    })
+    it('A internal test...', () => {
+
+    })
+    
+})
