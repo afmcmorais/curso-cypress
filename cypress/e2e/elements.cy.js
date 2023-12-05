02ca073d7b15cc47e834551a447f2e92ab1d4f24
André Felipe <afmcmorais@icloud.com>
2023-12-05 20:23:31 -0300

    test: Criando novo teste

--- cypress/e2e/elements.cy.js
+++ cypress/e2e/elements.cy.js
@@ -2,6 +2,7 @@ describe('Work with basic elements', () => {
     beforeEach(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
     })
+
     it('Text', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
@@ -12,10 +13,42 @@ describe('Work with basic elements', () => {
     it('Links', () => {
         cy.get('a').first().click()
         cy.get('#resultado').should('have.text', 'Voltou!')
+
         cy.reload()
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
 
+    it('TextFields', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 100 })
+            .should('have.value', 'acerto')
+    })
+
+    it('RadioButton', () => {
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
+
+        cy.get('#formSexoMasc')
+            .should('not.be.checked')
+
+        cy.get('[name=formSexo]').should('have.length', 2)
+    })
 })
