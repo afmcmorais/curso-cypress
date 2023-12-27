5398ff3fbee759df0925d0e6295c6be11d776ca6
André Felipe <afmcmorais@icloud.com>
2023-12-27 18:59:50 -0300

    Alert complete

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return {
+        browsers: config.browsers.filter(browser => browser.name !== 'electron')
+      }
     },
   },
   //defaultCommandTimeout: 1000
