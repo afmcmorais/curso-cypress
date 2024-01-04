f653428f48684b48928f542360e620b6e9494f3e
André Felipe <afmcmorais@icloud.com>
2024-01-04 19:58:25 -0300

    Locators Complete

--- cypress/e2e/popup.cy.js
+++ cypress/e2e/popup.cy.js
@@ -8,7 +8,7 @@ describe('Work with Popup', () => {
         })
     })
 
-    it.only('Deve verificar se o popup foi invocado', () => {
+    it('Deve verificar se o popup foi invocado', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.window().then(win => {
             cy.stub(win, 'open').as('winOpen')
@@ -17,5 +17,31 @@ describe('Work with Popup', () => {
         cy.get('@winOpen').should('be.called')
     })
 
+    describe.only('With links...', () => {
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
 
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should acess popup dinamcally', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield').type('funciona')
+            })
+
+        })
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+                cy.get('#tfield').type('funciona')
+        })
+    })
 })
