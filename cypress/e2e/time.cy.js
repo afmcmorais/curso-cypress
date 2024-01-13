e057850e438beb25862ceddb7b554ec68e639658
André Felipe <afmcmorais@icloud.com>
2024-01-12 21:40:48 -0300

    Clock module started

---
+++ cypress/e2e/time.cy.js
@@ -0,0 +1,9 @@
+describe('Work with alerts', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    it('Going back to the past', () => {
+        
+    })
+})
