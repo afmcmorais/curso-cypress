6a117f815c7034f8958561f1af65f9ab898e8db3
André Felipe <afmcmorais@icloud.com>
2024-01-14 16:48:08 -0300

    Clock module completed

--- cypress/e2e/time.cy.js
+++ cypress/e2e/time.cy.js
@@ -4,6 +4,16 @@ describe('Work with alerts', () => {
     })
 
     it('Going back to the past', () => {
-        
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '14/01/2024')
+
+        // cy.clock()
+        // cy.get('#buttonNow').click()
+        // cy.get('#resultado > span').should('contain', '31/12/1969')
+
+        const dt = new Date(2012, 3, 10, 15, 23, 50)
+        cy.clock(dt.getTime())
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '10/04/2012')
     })
 })
