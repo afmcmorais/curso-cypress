07bc4a5956feab290acc13d5f8fa8eaa8a145bbe
André Felipe <afmcmorais@icloud.com>
2024-01-14 17:11:05 -0300

    Tick ​​module completed

--- cypress/e2e/time.cy.js
+++ cypress/e2e/time.cy.js
@@ -16,4 +16,42 @@ describe('Work with alerts', () => {
         cy.get('#buttonNow').click()
         cy.get('#resultado > span').should('contain', '10/04/2012')
     })
+
+    it.only('Goes to the future', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '17052')
+        cy.get('#resultado > span').invoke('text').then(t => {
+            const number = parseInt(t)
+            cy.wrap(number).should('gt', 1705262187295)
+        })
+        
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(t => {
+            const number = parseInt(t)
+            cy.wrap(number).should('lte', 0)
+        })
+        
+        // cy.wait(1000)
+        // cy.get('#buttonTimePassed').click()
+        // cy.get('#resultado > span').invoke('text').then(t => {
+        //     const number = parseInt(t)
+        //     cy.wrap(number).should('lte', 1000)
+        // })
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(t => {
+            const number = parseInt(t)
+            cy.wrap(number).should('gte', 5000)
+        })
+
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').then(t => {
+            const number = parseInt(t)
+            cy.wrap(number).should('gte', 15000)
+        })
+        
+    })
 })
