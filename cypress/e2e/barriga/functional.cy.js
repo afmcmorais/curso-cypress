07fcea71b32a806f2838c03f139a912822b1dcac
André Felipe <afmcmorais@icloud.com>
2024-01-14 18:36:07 -0300

    Change account

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -1,5 +1,5 @@
 describe('Should test at a functional level', () => {
-    before(() => {
+    beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
         cy.get('.input-group > .form-control').type('andrefmorais@live.com')
         cy.get(':nth-child(2) > .form-control').type('153941')
@@ -11,7 +11,18 @@ describe('Should test at a functional level', () => {
         cy.get('[data-test="menu-settings"]').click()
         cy.get('[href="/contas"]').click()
         cy.get('[data-test="nome"]').type('Conta de teste')
-        cy.get('.btn')
+        cy.get('.btn').click()
         cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    })
+
+    it('Should update an account', () => {
+        cy.get('[data-test="menu-settings"]').click()
+        cy.get('[href="/contas"]').click()
+        cy.xpath("//table//td[contains(., 'Conta de teste')]/..//i[@class='far fa-edit']").click()
+        cy.get('[data-test="nome"]')
+            .clear()
+            .type('Conta alterada')
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso')
     })
 })
