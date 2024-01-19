f38d69bf4e18e600bd111a8a897a9b28ebb94542
André Felipe <afmcmorais@icloud.com>
2024-01-19 19:25:45 -0300

    Insert movement module completed

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -30,6 +30,19 @@ describe('Should test at a functional level', () => {
         cy.acessarMenuConta()
         cy.inserirConta('Conta alterada')
         cy.get(loc.MESSAGE).should('contain', 'code 400')
+    })
 
+    it('Should create a transaction', () => {
+        cy.login('andrefmorais@live.com', '153941')
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
     })
 })
