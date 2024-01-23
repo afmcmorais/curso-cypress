fd5141a774b77683169fb23ef4539ba8b5e41c80
André Felipe <afmcmorais@icloud.com>
2024-01-23 16:50:45 -0300

    Completed Balance Module

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -10,14 +10,14 @@ describe('Should test at a functional level', () => {
 
     it('Should creat an account', () => {
         cy.acessarMenuConta()
-        cy.inserirConta('Conta de teste')       
+        cy.inserirConta('Conta de teste')
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
     it('Should update an account', () => {
         cy.login('andrefmorais@live.com', '153941')
         cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.XP_BTN_ALETRAR).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALETRAR('Conta de teste')).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')
@@ -39,10 +39,18 @@ describe('Should test at a functional level', () => {
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
 
         cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
-        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc','123')).should('exist')
+    })
+
+    it('Should get balance', () => {
+        cy.login('andrefmorais@live.com', '153941')
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '123,00')
     })
 })
